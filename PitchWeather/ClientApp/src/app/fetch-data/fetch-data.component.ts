d867dea060a7898931f693a02b62b5c390ade4b6
Tengle22 <Trevor.Engle@gmail.com>
2020-05-11 22:45:14 -0400

    Example API

--- PitchWeather/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ PitchWeather/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -6,20 +6,66 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './fetch-data.component.html'
 })
 export class FetchDataComponent {
-  public forecasts: WeatherForecast[];
+  public weather: WeatherResponse;
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>('api.openweathermap.org/data/2.5/weather?zip=19382,US&appid=af380e2dc4a21a7a6f4a1499de0c8595').subscribe(result => {
+  constructor(http: HttpClient) {
+    http.get<WeatherResponse>('https://api.openweathermap.org/data/2.5/weather?q=Philadelphia&appid=7630b8ba707ebcfd349b1b9b97e52e99').subscribe(result => {
       console.log(result);
-      console.log('hi');
-      this.forecasts = result;
+      this.weather = result;
+      console.log(this.weather);
     }, error => console.error(error));
   }
 }
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+export interface Coord {
+  lon: number;
+  lat: number;
+}
+
+export interface Weather {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+export interface Main {
+  temp: number;
+  feels_like: number;
+  temp_min: number;
+  temp_max: number;
+  pressure: number;
+  humidity: number;
+}
+
+export interface Wind {
+  speed: number;
+  deg: number;
+}
+
+export interface Clouds {
+  all: number;
+}
+
+export interface Sys {
+  type: number;
+  id: number;
+  country: string;
+  sunrise: number;
+  sunset: number;
+}
+
+export interface WeatherResponse {
+  coord: Coord;
+  weather: Weather[];
+  base: string;
+  main: Main;
+  wind: Wind;
+  clouds: Clouds;
+  dt: number;
+  sys: Sys;
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
 }
